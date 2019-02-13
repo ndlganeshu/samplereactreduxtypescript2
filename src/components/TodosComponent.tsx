2c688ea38a5384d9a0aa4ac3307e195a31075cb1
VamshidharReddyEnumula <vamshidharenumula@gmail.com>
2019-02-13 12:16:44 +0530

    good commit.

--- src/components/TodosComponent.tsx
+++ src/components/TodosComponent.tsx
@@ -6,7 +6,7 @@ import Todos from "../containers/TodosContainer";
 export default class TodosComponent extends React.Component{
 
   render() {
-
+//https://github.com/Nemak121/react-redux-todo-ts
     return (
       <div>
         <h1>Todo List....</h1>
