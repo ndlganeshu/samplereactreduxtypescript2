b72c0f7af3fe7eea3e54faa7fcbdfe3db06e3eff
Ganesh Rao <ndlganeswararao@gmail.com>
2019-03-07 08:17:43 +0530

    More advanced .

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,12 @@
 import logger from 'redux-logger';
 import { State, reducer, initialState} from '../reducers';
-import {createStore, applyMiddleware, compose} from 'redux';
-const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+import {createStore, applyMiddleware} from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const composeEnhancers = composeWithDevTools({
+    // Specify name here, actionsBlacklist, actionsCreators and other options if needed
+  });
+
 const store = createStore<State, any, any, any>(reducer,initialState, composeEnhancers(applyMiddleware(logger)));
 
 export default store;
