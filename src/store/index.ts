29a957dd1e409d4e92a6ef5abfd93bb6eb4e4208
Ganesh Rao <ndlganeswararao@gmail.com>
2019-03-07 08:00:00 +0530

    Added the redux store extension to track in the browser redux state.

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,7 @@
 import logger from 'redux-logger';
 import { State, reducer, initialState} from '../reducers';
-import {createStore, applyMiddleware} from 'redux';
-
-const store = createStore<State, any, any, any>(reducer,initialState, applyMiddleware(logger));
+import {createStore, applyMiddleware, compose} from 'redux';
+const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore<State, any, any, any>(reducer,initialState, composeEnhancers(applyMiddleware(logger)));
 
 export default store;
