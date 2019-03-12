31700e4b333333d7a1716ed823dd7b35d0a9b19b
Bangarraju Gelli Gelli <v-bagell@microsoft.com>
2019-03-12 19:20:44 +0530

    Integrated the office fabric

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import * as React from 'react';
 import './App.css';
 // import Hello from './components/Hello';
 import TodosComponent from './components/TodosComponent';
+import Exa from './components/fabric/exa';
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
+      <Exa />
       <TodosComponent />
        {/* <Hello name="Ganesh" enthusiasmLevel={10}/> */}
       </div>
